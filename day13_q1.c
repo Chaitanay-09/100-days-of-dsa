65dc244435c3028214bd07d3fb4584258c02ea01
chaitanay <chaitanaygautam@gmail.com>
2026-02-15 17:11:14 +0530

    Add files via upload

---
+++ day13_q1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    scanf("%d %d", &r, &c);
+    
+    int matrix[100][100];  // Assuming max 100x100
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    
+    int top = 0, bottom = r - 1;
+    int left = 0, right = c - 1;
+    
+    while (top <= bottom && left <= right) {
+        // Top row
+        for (int i = left; i <= right; i++) {
+            printf("%d ", matrix[top][i]);
+        }
+        top++;
+        
+        // Right column
+        for (int i = top; i <= bottom; i++) {
+            printf("%d ", matrix[i][right]);
+        }
+        right--;
+        
+        // Bottom row (if needed)
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--) {
+                printf("%d ", matrix[bottom][i]);
+            }
+            bottom--;
+        }
+        
+        // Left column (if needed)
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                printf("%d ", matrix[i][left]);
+            }
+            left++;
+        }
+    }
+    
+    return 0;
+}
