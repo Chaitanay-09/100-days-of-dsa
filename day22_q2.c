305e088415cae73813f64eeb0a503456cc94c670
chaitanay <chaitanaygautam@gmail.com>
2026-02-23 09:14:28 +0530

    Add files via upload

---
+++ day22_q2.c
@@ -0,0 +1,14 @@
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode *prev = NULL;
+    struct ListNode *current = head;
+    struct ListNode *next = NULL;
+
+    while (current != NULL) {
+        next = current->next;   // store next node
+        current->next = prev;   // reverse link
+        prev = current;         // move prev forward
+        current = next;         // move current forward
+    }
+
+    return prev;  // new head
+}
